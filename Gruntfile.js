cffccb2e2cf1f27df4c995e374b92a8bdbfc1c70
Archon <den_archangel@mail.ru>
2017-01-04 06:05:32 +0500

    начал стилизацию

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,152 +1,59 @@
+"use strict";
+
 module.exports = function(grunt) {
-  
-  require("load-grunt-tasks")(grunt);
-  
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-sass");
+
   grunt.initConfig({
     sass: {
       style: {
         files: {
-          "build/css/style.css": "sass/style.scss"
+          "css/style.css": "sass/style.scss"
         }
       }
     },
+
     postcss: {
       style: {
         options: {
           processors: [
-            require("autoprefixer")({browsers:
-              [
-                "last 1 version",
-                "last 2 Chrome versions",
-                "last 2 Firefox versions",
-                "last 2 Opera versions",
-                "last 2 Edge versions"
-              ]}),
-            require("css-mqpacker")({
-              sort: true
-            })
+            require("autoprefixer")({browsers: [
+              "last 2 versions"
+            ]})
           ]
         },
-        src: "build/css/*.css"
+        src: "css/*.css"
       }
     },
-    watch: {
-      html: {
-        files: ["*.html"],
-        tasks: ["copy:html"]
-      },
-      style: {
-        files: ["sass/**/*.{scss,sass}"],
-        tasks: ["sass", "postcss", "csso"]
-      }
-    },
-    csso: {
-      style: {
-        options: {
-          report: "gzip"
-        },
-        files: {
-          "build/css/style.min.css": ["build/css/style.css"]
-        }
-      }
-    },
-    imagemin: {
-      images: {
-        options: {
-          optimizationLevel: 3
-        },
-        files: [{
-          expand: true,
-          src: ["build/img/*.{png,jpg,gif}"]
-        }]
-      }
-    },
-    svgstore: {
-      options: {
-        svg: {
-          style: "display: none"
-        }
-      },
-      symbols: {
-        files: {
-          "build/img/symbols.svg": ["img/icons/*.svg"]
-        }
-      }
-    },
-    svgmin: {
-      symbols: {
-        files: [{
-          expand: true,
-          src: ["build/img/*.svg"]
-        }]
-      }
-    },
-    csscomb: {
-      dist: {
-        options: {
-          config: 'csscomb.json'
-        },
-        files: {
-          "css/style.css": ['css/style.css']
-        }
-      }
-    },
-    copy: {
-      build: {
-        files: [{
-          expand: true,
-          src: [
-            "fonts/**",
-            "img/**",
-            "js/**",
-            "*.html"
-          ],
-          dest: "build"
-        }]
-      },
-      html: {
-        files: [{
-          expand: true,
-          src: ["*.html"],
-          dest: "build"
-        }]
-      }
-    },
-    clean: {
-      build: ["build"]
-    },
+
     browserSync: {
       server: {
         bsFiles: {
-          src: ["build/*.html", "build/css/*.css"]
+          src: [
+            "*.html",
+            "css/*.css"
+          ]
         },
         options: {
-          server: "build",
-          watchTask: true
+          server: ".",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
         }
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["sass/**/*.{scss,sass}"],
+        tasks: ["sass", "postcss"]
       }
     }
   });
-  
+
   grunt.registerTask("serve", ["browserSync", "watch"]);
-  grunt.registerTask("symbols", ["svgmin", "svgstore"]);
-  
-  /* grunt.registerTask("build", [
-    "csscomb",
-    "clean",
-    "copy",
-    "sass",
-    "postcss",
-    "csso",
-    "symbols",
-    "imagemin"
-  ]); */
-    grunt.registerTask("build", [
-    "clean",
-    "copy",
-    "sass",
-    "postcss"
-  ]);
 };
-
-
